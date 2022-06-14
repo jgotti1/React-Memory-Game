187bdae2ce36b96b34d86ee07603fdb20988adc5
jgotti1 <96301204+jgotti1@users.noreply.github.com>
2022-06-14 16:03:53 -0400

    first commit creat randow card array

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
+const cardImages = [
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
+];
 function App() {
+  // mix up cards
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages].sort(() => Math.random() - 0.5).map((card) => ({ ...card, id: Math.random() }));
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+  console.log(cards, turns);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Match Game</h1>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
